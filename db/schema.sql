f257f443249e5b741b71ef0a2a95157a78edadfa
NatJoshYoyo <joshuaobrien28@yahoo.com>
2023-02-06 16:23:19 -0500

    Made tables for schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
-Use employee_db;
+USE employee_db;
 
 CREATE TABLE departments (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -9,5 +9,20 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    
-)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    departments_id INT NOT NULL,
+    FOREIGN KEY (departments_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    roles_id INT NOT NULL,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id)
+);
